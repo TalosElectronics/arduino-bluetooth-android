1a668ea5e81d29bda86118064f28c6c078520284
Benn Sandoval <bennsandoval>
2015-08-08 17:12:51 -0500

    First commit

---
+++ Foco_Rele_Modulo_bluetooth.ino
@@ -0,0 +1,24 @@
+int Carga=13;
+int estado=0;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(Carga,OUTPUT);
+}
+
+void loop(){
+ if(Serial.available()>0){
+ estado = Serial.read();
+ }
+ 
+if (estado =='1')
+  {
+   digitalWrite(Carga,HIGH);
+  }
+if(estado=='2')
+  {
+   digitalWrite(Carga,LOW);
+  }
+  
+}
